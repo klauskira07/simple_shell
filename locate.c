9fd28eef1ec6bf9aff7865a31b006171c121f910
Klaus KIRA <klauskira07@gmail.com>
2022-08-12 14:59:29 -0600

    locate

---
+++ locate.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
+
+/**
+ * get_location - Locates a command in the PATH.
+ * @command: The command to locate.
+ *
+ * Return: If an error occurs or the command cannot be located - NULL.
+ *         Otherwise - the full pathname of the command.
+ */
+char *get_location(char *command)
+{
+	char **path, *temp;
+	list_t *dirs, *head;
+	struct stat st;
+
+	path = _getenv("PATH");
+	if (!path || !(*path))
+		return (NULL);
+
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
+
+	while (dirs)
+	{
+		temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!temp)
+			return (NULL);
+
+		_strcpy(temp, dirs->dir);
+		_strcat(temp, "/");
+		_strcat(temp, command);
+
